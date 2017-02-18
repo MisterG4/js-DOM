fb389eecd806cd7f43295046a15bf59390b1a8f9
Anthony Garingan <anthonygaringan@Anthony-Garigan.local>
2017-02-17 21:16:10 -1000

    Bruce lee

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
 /*1. Replace the n/a with the following:
 
 Tay-Tay*/
+var changeName = document.getElementById("name1");
+changeName.innerHTML = "Tay-Tay";
 
 
 
@@ -10,17 +12,21 @@ Tay-Tay*/
 
 Project Manager*/
 
-
+var newManager = document.getElementById("position2");
+newManager.innerHTML = "Project Manager";
 
 /*3. Replace the n/a with the following:
 
 Concatenation*/
 
+var pikoTaro = document.getElementById("alias3");
+pikoTaro.innerHTML = "Concatenation";
 
 
 /*4. Replace the n/a for Snoop with the gangsta lorem ipsum*/
 
-
+var snoopDogg = document.getElementsByClassName("profile");
+snoopDogg[0].innerHTML = "Lorizzle ipsizzle dolizzle sit amizzle, consectetuer adipiscing yo mamma. Nullam sapien velizzle, its fo rizzle volutpizzle, suscipit for sure, brizzle vizzle, its fo rizzle. Pellentesque we gonna chung tortizzle. Sed eros. Stuff fizzle dolor dapibus turpizzle tempizzle shizznit. pellentesque nibh et turpizzle. Vestibulum izzle tortor. Gangsta mammasay mammasa mamma oo sa rhoncus fo shizzle. Izzle the bizzle habitasse bow wow wow dictumst. Dang dapibizzle. I’m in the shizzle we gonna chung urna, pretizzle eu, mattis mah nizzle, eleifend phat, nunc. Stuff suscipizzle. Integer sempizzle velit sizzle mofo."
 
 /*5. Relace the n/a for Samuel L Jackson with his lorem ipsum*/
 
